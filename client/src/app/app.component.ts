dd006ee8e18f900a348a084afa5b4809947b88a9
Elizaveta Pashkovskaya <elizaveta@Elizaveta-MacBook-Pro.local>
2024-12-05 14:44:04 +0200

    add ssl cert

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { CommonModule } from '@angular/common';
 
