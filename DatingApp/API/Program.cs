0655b083b61c8dbc937ea82fdd4899f67d45507d
Elizaveta Pashkovskaya <elizavetapashkovskaya@Elizavetas-MacBook-Air.local>
2024-12-18 15:02:59 +0200

    add comments into Program file

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -2,14 +2,21 @@ using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+// Ensure CORS is configured before Authentication and Authorization
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+// Authentication and Authorization should come after CORS configuration
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
